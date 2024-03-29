5ae1c4ad0c29a8e5360b4670d46aeae874d0c59c
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-03-29 20:39:18 -0300

    chore: injects testing env on conftest app fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,15 +10,15 @@ def create_initial_clients():
     initial_limits = [100000, 80000, 1000000, 10000000, 500000]
 
     for limit in initial_limits:
-        client = Client(limit=limit, balance=0)
+        client = Client(limit_of=limit, balance=0)
         db.session.add(client)
 
     db.session.commit()
 
 
-@fixture(scope="session")
+@fixture()
 def app() -> Generator:
-    app = create_app(FORCE_ENV_FOR_DYNACONF="testing")
+    app = create_app("testing")
 
     with app.app_context():
         db.create_all()
@@ -29,7 +29,7 @@ def app() -> Generator:
         db.drop_all()
 
 
-@fixture(scope="session")
+@fixture()
 def client(app: Flask) -> Generator:
     with app.test_client() as client:
         yield client
