0328a2148cea6c03b750ac1c2862e93e48d912fe
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-03-29 20:41:12 -0300

    chore: runs create-database and create-clients cli on dev_server sh

--- run_dev_server.sh
+++ run_dev_server.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+python -m flask cli create-database
+
 python -m flask db upgrade
 
-python -m flask --app rinha_de_backend_2024_q1/app run --debug --host=0.0.0.0
+python -m flask cli create-clients
+
+python -m flask run --host=0.0.0.0
