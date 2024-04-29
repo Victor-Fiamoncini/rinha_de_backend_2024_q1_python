0d5b361fafe3732022878c7dfa65b4acc71c97f1
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2024-04-29 01:58:40 -0300

    feat: adds transaction get-without-owner method

--- rinha_de_backend_2024_q1/domain/entities/transaction_entity.py
+++ rinha_de_backend_2024_q1/domain/entities/transaction_entity.py
@@ -1,30 +1,34 @@
 from dataclasses import dataclass
+from datetime import datetime
 from rinha_de_backend_2024_q1.domain.exceptions import InconsistentBalanceException
 
 
 @dataclass(frozen=True)
 class MakeNewInput:
-    value: int
-    type_of: str
     description: str
     owner: "ClientEntity"
+    type_of: str
+    value: int
+
+
+@dataclass(frozen=True)
+class GetWithoutOwnerInput:
+    created_at: datetime
+    description: str
+    type_of: str
+    value: int
 
 
 class TransactionEntity:
-    def __init__(
-        self,
-        value: int,
-        type_of: str,
-        description: str,
-        owner: "ClientEntity",
-    ):
-        self.value = value
-        self.type_of = type_of
-        self.description = description
-        self.owner = owner
+    def __init__(self):
+        self.created_at = None
+        self.description = None
+        self.owner = None
+        self.type_of = None
+        self.value = None
 
     @staticmethod
-    def make_new(input: MakeNewInput):
+    def make_new(input: MakeNewInput) -> "TransactionEntity":
         if input.type_of == "d":
             new_balance = input.owner.balance - input.value
 
@@ -33,12 +37,28 @@ class TransactionEntity:
                     "The amount debited will exceed the client's limit"
                 )
 
-        return TransactionEntity(
-            value=input.value,
-            type_of=input.type_of,
-            description=input.description,
-            owner=input.owner,
+        return (
+            TransactionEntityBuilder()
+            .add_description(input.description)
+            .add_owner(input.owner)
+            .add_type_of(input.type_of)
+            .add_value(input.value)
+            .build()
+        )
+
+    @staticmethod
+    def get_without_owner(input: GetWithoutOwnerInput) -> "TransactionEntity":
+        return (
+            TransactionEntityBuilder()
+            .add_created_at(input.created_at)
+            .add_description(input.description)
+            .add_type_of(input.type_of)
+            .add_value(input.value)
+            .build()
         )
 
 
 from rinha_de_backend_2024_q1.domain.entities.client_entity import ClientEntity
+from rinha_de_backend_2024_q1.domain.builders.transaction_entity_builder import (
+    TransactionEntityBuilder,
+)
